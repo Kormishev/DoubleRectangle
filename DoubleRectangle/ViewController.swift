26ba448c8d9d8a54d169ee86bc15c46dbb80f501
Kormishev Mike <kormishev@bk.ru>
2023-07-19 18:12:52 +0300

    Delete template comment

--- DoubleRectangle/ViewController.swift
+++ DoubleRectangle/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  DoubleRectangle
-//
-//  Created by Mike on 18.07.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
